11453e80c545248daa7fb895d9cab96a008c0063
Kvazar7 <Kometa737@gmail.com>
2023-06-25 10:00:16 +0300

    hw-04

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import { addContact } from '../../controlers/addContact.js';
-import { delContact } from '../../controlers/delContact.js';
-import { getAllContacts } from '../../controlers/getAllContact.js';
-import { getContactById } from '../../controlers/getContactById.js';
-import { updateFavorite } from '../../controlers/updateContactStatus.js';
-import { updateContact } from '../../controlers/updateContact.js';
+import { addContact } from '../../controlers/contacts/addContact.js';
+import { delContact } from '../../controlers/contacts/delContact.js';
+import { getAllContacts } from '../../controlers/contacts/getAllContact.js';
+import { getContactById } from '../../controlers/contacts/getContactById.js';
+import { updateFavorite } from '../../controlers/contacts/updateContactStatus.js';
+import { updateContact } from '../../controlers/contacts/updateContact.js';
 
 import { isValidId } from '../../helpers/validateById.js';
 import { validateBody } from '../../helpers/validateBody.js';
@@ -14,15 +14,10 @@ import { schemas } from '../../schemas/contacts.js';
 const router = Router();
 
 router.get('/', getAllContacts);
-
 router.get('/:contactId', isValidId, getContactById);
-
 router.post('/', validateBody(schemas.addSchema), addContact);
-
 router.delete('/:contactId', isValidId, delContact);
-  
 router.put('/:contactId', validateBody(schemas.addSchema), isValidId, updateContact);
-
 router.patch('/contacts/:id/favorite', isValidId, validateBody(schemas.updateFavoriteSchema), updateFavorite);
 
 export default router
